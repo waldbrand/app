098a43b022f18de03c90502f3e7c75f6f8a64d5b
Sebastian Kürten <sebastian@topobyte.de>
2021-06-11 16:34:03 +0200

    Update license headers

--- app/src/main/java/de/topobyte/apps/viewer/diacritic/DiacriticUtil.java
+++ app/src/main/java/de/topobyte/apps/viewer/diacritic/DiacriticUtil.java
@@ -1,19 +1,19 @@
 // Copyright 2021 Sebastian Kuerten
 //
-// This file is part of stadtplan-app.
+// This file is part of waldbrand-app.
 //
-// stadtplan-app is free software: you can redistribute it and/or modify
+// waldbrand-app is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// stadtplan-app is distributed in the hope that it will be useful,
+// waldbrand-app is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with stadtplan-app. If not, see <http://www.gnu.org/licenses/>.
+// along with waldbrand-app. If not, see <http://www.gnu.org/licenses/>.
 
 package de.topobyte.apps.viewer.diacritic;
 

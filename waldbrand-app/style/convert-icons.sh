14edbff850639e89c923a88e58ad0a5e7bd8b0f2
Sebastian Kürten <sebastian@topobyte.de>
2021-10-11 07:55:02 +0200

    Use new template icon for hydrants withouth fixed 'H'

--- waldbrand-app/style/convert-icons.sh
+++ waldbrand-app/style/convert-icons.sh
@@ -7,6 +7,7 @@ MARKERS="$MATERIAL/marker/vectorized-font"
 
 SvgToBvg "$MARKERS/hydrant.svg" "$DIR/default/symbols/hydrant.bvg"
 SvgToBvg "$MARKERS/hydrant-unklar.svg" "$DIR/default/symbols/hydrant-unklar.bvg"
+SvgToBvg "$MARKERS/hydrant-template.svg" "$DIR/default/symbols/hydrant-template.bvg"
 SvgToBvg "$MARKERS/speicher.svg" "$DIR/default/symbols/speicher.bvg"
 SvgToBvg "$MARKERS/tiefbrunnen.svg" "$DIR/default/symbols/tiefbrunnen.bvg"
 SvgToBvg "$MARKERS/saugstelle.svg" "$DIR/default/symbols/saugstelle.bvg"
